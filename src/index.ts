f80737d8edf23d4233128b89576a80297a5b1d4a
James Campbell <james@supmenow.com>
2018-05-11 22:29:46 +0100

    Remove check.

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,3 @@ export * from './eventline'
 export * from './get-capture-groups'
 export * from './when'
 export * from './matchers'
-
-/*
-Warn the consumer that Eventline only supports the new RxJS if we detect the old one
-is installed.
-
-Hopefully this will save on headaches.
-*/
-try {
-    console.log(require.resolve("rx"));
-    console.error("Eventline detected an old version of RxJS is installed. Eventline only supports version 5 and above. Please ensure you use the correct version in your code if you are having problems with Eventline.");
-} catch(_) {}
