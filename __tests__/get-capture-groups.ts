5a329c0fcc6ac63578e2435c89fef652858c9fee
James Campbell <james@supmenow.com>
2018-05-11 23:07:04 +0100

    Enableing tests for bits not effected by rx change.

--- __tests__/get-capture-groups.ts
+++ __tests__/get-capture-groups.ts
@@ -1,43 +1,43 @@
-// import { getCaptureGroups } from '../src/get-capture-groups'
-// import * as XRegExp from 'xregexp'
+import { getCaptureGroups } from '../src/get-capture-groups'
+import * as XRegExp from 'xregexp'
 
-// test('Returns anonymous capture groups for regular expression from event keypath', () => {
+test('Returns anonymous capture groups for regular expression from event keypath', () => {
 
-//   let regexp = /hello(\d+)/
-//   let event = {
-//     'pattern': {
-//       'text': regexp  
-//     },
-//     'text': 'hello1'
-//   }
-//   let result = getCaptureGroups(event, 'text')
+  let regexp = /hello(\d+)/
+  let event = {
+    'pattern': {
+      'text': regexp  
+    },
+    'text': 'hello1'
+  }
+  let result = getCaptureGroups(event, 'text')
 
-//   expect(result[1]).toEqual('1');
-// });
+  expect(result[1]).toEqual('1');
+});
 
-// test('Returns named capture groups for regular expression from event keypath', () => {
-//   let regexp = new XRegExp('hello(?<number>\\d+)')
-//   let event = {
-//     'pattern': {
-//       'text': regexp  
-//     },
-//     'text': 'hello1'
-//   }
-//   let result = getCaptureGroups(event, 'text')
+test('Returns named capture groups for regular expression from event keypath', () => {
+  let regexp = new XRegExp('hello(?<number>\\d+)')
+  let event = {
+    'pattern': {
+      'text': regexp  
+    },
+    'text': 'hello1'
+  }
+  let result = getCaptureGroups(event, 'text')
 
-//   expect(result.number).toEqual('1');
-// });
+  expect(result.number).toEqual('1');
+});
 
-// test('Returns anonymous capture groups for regular expression in array from event keypath', () => {
+test('Returns anonymous capture groups for regular expression in array from event keypath', () => {
 
-//   let regexp = [/hello(\d+)/, /bye(\d+)/]
-//   let event = {
-//     'pattern': {
-//       'text': regexp  
-//     },
-//     'text': 'bye1'
-//   }
-//   let result = getCaptureGroups(event, 'text')
+  let regexp = [/hello(\d+)/, /bye(\d+)/]
+  let event = {
+    'pattern': {
+      'text': regexp  
+    },
+    'text': 'bye1'
+  }
+  let result = getCaptureGroups(event, 'text')
 
-//   expect(result[1]).toEqual('1');
-// });
+  expect(result[1]).toEqual('1');
+});
